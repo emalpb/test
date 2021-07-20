b3b4ab7cb0bb90732688eb2cc7182c4a11ab10a1
emaderito <emanuelderito@gmail.com>
2021-07-20 13:29:37 -0300

    time

--- creation.tf
+++ creation.tf
@@ -1,22 +1,26 @@
 resource "aws_s3_bucket" "onebucket" {
-   bucket = "test-prueba-derito"
-   acl = "private"
-   versioning {
-      enabled = true
-   }
-   tags = {
-     Name = "Flugel-test"
-     Environment = "Dev"
-   }
+  bucket = "test-prueba-derito"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  tags = {
+    Name        = "Flugel-test"
+    Environment = "Dev"
+  }
+}
+
+data "local_exe" "time" {
+    program=["bash","date"]	
 }
 
 resource "local_file" "test1" {
-    content     = "foo!"
-    filename = "${path.module}/test1.txt"
+  content  = data.local_exe.time
+  filename = "${path.module}/test1.txt"
 }
 
 resource "local_file" "test2" {
-    content     = "foo!"
-    filename = "${path.module}/test2.txt"
+  content  = data.local_exe.time
+  filename = "${path.module}/test2.txt"
 }
 
