4ff38d2856dc099cb5982251b11195496982fbef
emaderito <emanuelderito@gmail.com>
2021-07-21 21:53:47 -0300

    update

--- README.md
+++ README.md
@@ -2,5 +2,5 @@ Terraform + Github Actions
 
 Push in dev runs Terratest 
 
-PR to master runs Terraform main.tf
+PR to master runs Terraform main.tf when is approved
 
--- main.tf
+++ main.tf
@@ -8,162 +8,221 @@ provider "aws" {
    secret_key = var.aws_secret_key
    region     = var.aws_region
 }
+# bucket
+resource "aws_s3_bucket" "onebucket" {
+  bucket = "test-prueba-derito"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  tags = {
+    Name        = "Flugel-test"
+    Environment = "Dev"
+  }
+}
+# file 1
+resource "null_resource" "test1" {
+  provisioner "local-exec" {
+    command = "date '+%H%M-%d-%m-%Y' >> ${path.module}/test1.txt"
+  }
+}
+# file 2
+resource "null_resource" "test2" {
+  provisioner "local-exec" {
+    command = "date '+%H%M-%d-%m-%Y' >> ${path.module}/test2.txt"
+  }
+}
+# upload files 1/2
+resource "aws_s3_bucket_object" "object" {
+  for_each = fileset("${path.module}", "*.txt")
 
-# resource "aws_s3_bucket" "onebucket" {
-#   bucket = "test-prueba-derito"
-#   acl    = "private"
-#   versioning {
-#     enabled = true
-#   }
-#   tags = {
-#     Name        = "Flugel-test"
-#     Environment = "Dev"
-#   }
-# }
+  bucket = "test-prueba-derito"
+  key    = each.value
+  source = "${path.module}/${each.value}"
+  etag   = filemd5("${each.value}")
+}
+# vpc creation
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
 
-# resource "null_resource" "test1" {
-#   provisioner "local-exec" {
-#     command = "date '+%H%M-%d-%m-%Y' >> ${path.module}/test1.txt"
-#   }
-# }
+  tags = {
+    Name = "Flugel-test"
+  }
+}
 
-# resource "null_resource" "test2" {
-#   provisioner "local-exec" {
-#     command = "date '+%H%M-%d-%m-%Y' >> ${path.module}/test2.txt"
-#   }
-# }
+# oublic subnet
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.0.0/24"
+  availability_zone = "sa-east-1a"
 
-# resource "aws_s3_bucket_object" "object" {
-#   for_each = fileset("${path.module}", "*.txt")
+  tags = {
+    Name = "Flugel-test"
+  }
+}
 
-#   bucket = "test-prueba-derito"
-#   key    = each.value
-#   source = "${path.module}/${each.value}"
-#   etag   = filemd5("${each.value}")
-# }
+resource "aws_subnet" "my_subnet_private" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.1.0/24"
+  availability_zone = "sa-east-1a"
 
-# resource "aws_security_group" "instance" {
-# 	name = "terraform-tcp-security-group"
- 
-# 	ingress {
-# 		from_port = 80
-# 		to_port = 80
-# 		protocol = "tcp"
-# 		cidr_blocks = ["0.0.0.0/0"]
-# 	}
- 
-#     egress {
-#         from_port = 0
-#         to_port = 0
-#         protocol = "-1"
-#         cidr_blocks = ["0.0.0.0/0"]
-#     }
-# }
+  tags = {
+    Name = "Flugel-test"
+  }
+}
 
-# resource "aws_vpc" "my_vpc" {
-#   cidr_block = "172.16.0.0/16"
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.my_vpc.id
 
-#   tags = {
-#     Name = "Flugel-test"
-#   }
-# }
+  tags = {
+    Name = "Flugel-test"
+  }
+}
 
-# resource "aws_subnet" "my_subnet" {
-#   vpc_id            = aws_vpc.my_vpc.id
-#   cidr_block        = "172.16.10.0/24"
-#   availability_zone = "sa-east-1"
-#   map_public_ip_on_launch = true
-#   tags = {
-#     Name = "Flugel-test"
-#   }
-# }
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.my_vpc.id
 
-# resource "aws_network_interface" "flugel" {
-#   subnet_id   = aws_subnet.my_subnet.id
-#   private_ips = ["172.16.10.100"]
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "Flugel-test"
+  }
+}
 
-#   tags = {
-#     Name = "Flugel-test-primary_network_interface"
-#   }
-# }
+resource "aws_route_table" "example_private" {
+  vpc_id = aws_vpc.my_vpc.id
 
-# resource "aws_instance" "ec2_flugel_1" {
-#   ami           = "ami-05e809fbeee38dd5e" 
-#   instance_type = "t2.micro"
+  tags = {
+    Name = "Flugel-test"
+  }
+}
 
-#   network_interface {
-#     network_interface_id = aws_network_interface.flugel.id
-#     device_index         = 0
-#   }
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.my_subnet_private.id
+  route_table_id = aws_route_table.example_private.id
+}
 
-#   tags = {
-# 		Name = "flugel1"
-# 	}
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.example.id
+}
 
-#   vpc_security_group_ids = [aws_security_group.instance.id]
-# }
+resource "aws_network_interface" "one" {
+  subnet_id   = aws_subnet.my_subnet_private.id
+  private_ips = ["172.16.1.100"]
 
-# resource "aws_instance" "ec2_flugel_2" {
-#   ami           = "ami-05e809fbeee38dd5e" 
-#   instance_type = "t2.micro"
+  tags = {
+    Name = "primary_network_interface_one"
+  }
+}
 
-#   network_interface {
-#     network_interface_id = aws_network_interface.flugel.id
-#     device_index         = 0
-#   }
+resource "aws_network_interface" "two" {
+  subnet_id   = aws_subnet.my_subnet_private.id
+  private_ips = ["172.16.1.101"]
 
-#   tags = {
-# 		Name = "flugel2"
-# 	}
+  tags = {
+    Name = "primary_network_interface_two"
+  }
+}
+
+
+resource "aws_instance" "ec2_flugel_1" {
+  ami           = "ami-05e809fbeee38dd5e" 
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.one.id
+    device_index         = 0
+  }
+
+  tags = {
+		Name = "flugel1"
+	}
+
+  vpc_security_group_ids = [aws_security_group.instance.id]
+}
+
+resource "aws_instance" "ec2_flugel_2" {
+  ami           = "ami-05e809fbeee38dd5e" 
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.two.id
+    device_index         = 0
+  }
+
+  tags = {
+		Name = "flugel2"
+	}
   
-#   vpc_security_group_ids = [aws_security_group.instance.id]
-# }
+  vpc_security_group_ids = [aws_security_group.instance.id]
+}
 
-# resource "aws_lb" "alb_test" {
-#   name               = "test-flugel"
-#   internal           = false
-#   load_balancer_type = "application"
-#   security_groups    = [aws_security_group.instance.id]
-#   subnet_mapping {
-#     subnet_id = aws_subnet.my_subnet.id
-#   }            
+resource "aws_security_group" "instance" {
+	name = "terraform-tcp-security-group"
+ 
+	ingress {
+		from_port = 80
+		to_port = 80
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+ 
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
 
-#   enable_deletion_protection = true
+resource "aws_lb" "alb_test" {
+  name               = "test-flugel"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.instance.id]
+  subnet_mapping {
+    subnet_id = aws_subnet.my_subnet.id
+  }            
 
-#   tags = {
-#     Environment = "dev"
-#   }
-# }
+  enable_deletion_protection = true
 
-# resource "aws_lb_target_group" "alb_tg" {
-#  name     = "my-target-group"
-#  port     = 80
-#  protocol = "HTTP"
-#  vpc_id   = aws_vpc.my_vpc.id
-# }
+  tags = {
+    Environment = "dev"
+  }
+}
 
-# resource "aws_lb_target_group_attachment" "target_registration" {
-#   target_group_arn = aws_lb_target_group.alb_tg.arn
-#   target_id        = aws_instance.ec2_flugel_1.id
-#   port             = 80
-# }
+resource "aws_lb_target_group" "alb_tg" {
+ name     = "my-target-group"
+ port     = 80
+ protocol = "HTTP"
+ vpc_id   = aws_vpc.my_vpc.id
+}
 
-# resource "aws_lb_target_group_attachment" "target_registration2" {
-#   target_group_arn = aws_lb_target_group.alb_tg.arn
-#   target_id        = aws_instance.ec2_flugel_2.id
-#   port             = 80
-# }
+resource "aws_lb_target_group_attachment" "target_registration" {
+  target_group_arn = aws_lb_target_group.alb_tg.arn
+  target_id        = aws_instance.ec2_flugel_1.id
+  port             = 80
+}
 
-# resource "aws_lb_listener" "alb_listener" {
-#  load_balancer_arn = aws_lb.alb_test.arn
-#  port              = "80"
-#  protocol          = "HTTP"
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.alb_tg.arn
-#   }
-# }
+resource "aws_lb_target_group_attachment" "target_registration2" {
+  target_group_arn = aws_lb_target_group.alb_tg.arn
+  target_id        = aws_instance.ec2_flugel_2.id
+  port             = 80
+}
 
-# output "alb_dns" {
-#   value = aws_lb.alb_test.dns_name
-# }
+resource "aws_lb_listener" "alb_listener" {
+ load_balancer_arn = aws_lb.alb_test.arn
+ port              = "80"
+ protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_tg.arn
+  }
+}
+
+output "alb_dns" {
+  value = aws_lb.alb_test.dns_name
+}
